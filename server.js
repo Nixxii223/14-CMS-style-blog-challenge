69d2695559242f6d510332af0dbf467a4b2f4b5f
nickib223 <146459728+nickib223@users.noreply.github.com>
2024-03-04 23:26:16 -0500

    server.js code and file structure, handlebars, and routes

---
+++ server.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const expressHandlebars = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./Develop/config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const handlebars = expressHandlebars.create({ helpers });
+
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {
+      maxAge: 300000,
+      httpOnly: true,
+      secure: false,
+      sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
